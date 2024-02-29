42481d2686db339dc47b28b5bcd2068b179793f2
Svitlana Vasylkovska <vaselisssa@gmail.com>
2024-02-29 22:11:10 +0200

    change DB_HOST

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ require("colors");
 const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
-  .connect(DB_HOST)
-  .then(() => {
-    console.log("Database connection successful".bgBrightGreen);
-    app.listen(PORT);
-  })
-  .catch((err) => {
-    console.log(err);
-    process.exit(1);
-  });
+   .connect(DB_HOST)
+   .then(() => {
+      console.log("Database connection successful".bgBrightGreen);
+      app.listen(PORT);
+   })
+   .catch((err) => {
+      console.log(err);
+      process.exit(1);
+   });
