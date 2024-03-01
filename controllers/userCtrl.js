7ee0b94fb4eb69d0323f9b183eb7cb513497d3e4
Svitlana Vasylkovska <vaselisssa@gmail.com>
2024-03-01 17:14:12 +0200

    fix register

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,11 +1,10 @@
 const { HttpError, ctrlWrapper } = require("../helpers");
-
 const { User } = require("../models/UserModel");
 
 const {
    loginUserSrv,
    logoutUserSrv,
-   addUserSrv,
+   //  addUserSrv,
    currentUserSrv,
 } = require("../services");
 
@@ -41,22 +40,39 @@ class UsersController {
       // if (error) throw HttpError(400, error.message);
 
       //Проверка на уникальность Email
-      const user = await User.findOne({ email: req.body.email });
+      //     const user = await User.findOne({ email: req.body.email });
+
+      //     if (user) {
+      //        return res.status(409).json({
+      //           status: "error",
+      //           code: 409,
+      //           message: "Email is already in use",
+      //           data: "Conflict",
+      //        });
+      //     }
+
+      //     const { email } = await addUserSrv(req.body);
+
+      //     res.status(201).json({
+      //        user: {
+      //           email,
+      //        },
+      //     });
+      //  });
+
+      const { email } = req.body;
+      const user = await User.findOne({ email });
 
       if (user) {
-         return res.status(409).json({
-            status: "error",
-            code: 409,
-            message: "Email is already in use",
-            data: "Conflict",
-         });
+         throw HttpError(409, "Email in use");
       }
 
-      const { email } = await addUserSrv(req.body);
+      const newUser = await User.create(req.body);
 
       res.status(201).json({
          user: {
-            email,
+            name: newUser.name,
+            email: newUser.email,
          },
       });
    });
