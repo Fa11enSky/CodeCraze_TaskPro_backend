3ba89313d23cc5054bb5e3940f9c6da453fda2a8
Svitlana Vasylkovska <vaselisssa@gmail.com>
2024-03-01 18:38:47 +0200

    new ctr register

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -34,48 +34,31 @@ class UsersController {
       res.status(204).json();
    });
 
-   register = ctrlWrapper(async (req, res) => {
-      //Валидация
-      // const { error } = await User.validate(req.body);
-      // if (error) throw HttpError(400, error.message);
+   //  register = ctrlWrapper(async (req, res) => {
+   //Валидация
+   // const { error } = await User.validate(req.body);
+   // if (error) throw HttpError(400, error.message);
 
-      //Проверка на уникальность Email
-      //     const user = await User.findOne({ email: req.body.email });
+   //Проверка на уникальность Email
+   //     const user = await User.findOne({ email: req.body.email });
 
-      //     if (user) {
-      //        return res.status(409).json({
-      //           status: "error",
-      //           code: 409,
-      //           message: "Email is already in use",
-      //           data: "Conflict",
-      //        });
-      //     }
+   //     if (user) {
+   //        return res.status(409).json({
+   //           status: "error",
+   //           code: 409,
+   //           message: "Email is already in use",
+   //           data: "Conflict",
+   //        });
+   //     }
 
-      //     const { email } = await addUserSrv(req.body);
+   //     const { email } = await addUserSrv(req.body);
 
-      //     res.status(201).json({
-      //        user: {
-      //           email,
-      //        },
-      //     });
-      //  });
-
-      const { email } = req.body;
-      const user = await User.findOne({ email });
-
-      if (user) {
-         throw HttpError(409, "Email in use");
-      }
-
-      const newUser = await User.create(req.body);
-
-      res.status(201).json({
-         user: {
-            name: newUser.name,
-            email: newUser.email,
-         },
-      });
-   });
+   //     res.status(201).json({
+   //        user: {
+   //           email,
+   //        },
+   //     });
+   //  });
 
    current = ctrlWrapper(async (req, res, next) => {
       const { email, name, surname } = await currentUserSrv(req.user.token);
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -4,11 +4,12 @@ const UsersController = require("../controllers/userCtrl");
 const { protect, validateBody } = require("../middlewares");
 
 const { loginSchema, registerSchema } = require("../models/UserModel");
+const { register } = require("../controllers/auth");
 
 const usersRouter = express.Router();
 
 usersRouter
-   .post("/register", validateBody(registerSchema), UsersController.register)
+   .post("/register", validateBody(registerSchema), register)
    .post("/login", UsersController.login)
    .get("/logout", protect, UsersController.logout)
    .get("/current", protect, UsersController.current);
