7d0e2d1057aeb10ed962bbc4b3970335d7e56ef0
Svitlana Vasylkovska <vaselisssa@gmail.com>
2024-03-05 23:00:11 +0200

    add title-validation to addBoard

--- controllers/boards/addBoard.js
+++ controllers/boards/addBoard.js
@@ -5,12 +5,10 @@ const addBoard = ctrlWrapper(async (req, res) => {
    const { _id: owner } = req.user;
    const { title } = req.body;
 
-   console.log(title);
-
-   // const isBoardExists = await Board.findOne({ owner, title });
-   // if (isBoardExists) {
-   //    throw HttpError(404, `Board with ${title} already exist`);
-   // }
+   const isBoardExists = await Board.findOne({ owner, title });
+   if (isBoardExists) {
+      throw HttpError(404, `Board "${title}" already exist`);
+   }
    const result = await Board.create({ ...req.body, owner });
    res.status(201).json(result);
 });
