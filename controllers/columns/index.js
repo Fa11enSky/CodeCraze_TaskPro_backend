f6b876b6beb292bce5da68bcd57f8cf6c9ca82a4
Svitlana Vasylkovska <vaselisssa@gmail.com>
2024-03-08 00:11:21 +0200

    fixes

--- controllers/columns/index.js
+++ controllers/columns/index.js
@@ -1,13 +1,9 @@
 const addColumn = require("./addColumn");
-const getColumns = require("./getColumns");
-const getColumnById = require("./getColumnById");
 const deleteColumn = require("./deleteColumn");
 const updateColumn = require("./updateColumn");
 
 module.exports = {
-  addColumn,
-  getColumns,
-  getColumnById,
-  deleteColumn,
-  updateColumn,
+   addColumn,
+   deleteColumn,
+   updateColumn,
 };
--- routes/columnsRouter.js
+++ routes/columnsRouter.js
@@ -1,19 +1,18 @@
 const express = require("express");
 
 const {
-  addColumn, getColumns, getColumnById, deleteColumn, updateColumn,
-  //updateColumn,
-  //deleteColumn,
+   addColumn,
+   deleteColumn,
+   updateColumn,
 } = require("../controllers/columns");
 const { protect, validateBody } = require("../middlewares");
 const { addColumnSchema, updateColumnSchema } = require("../schemas");
 
 const columnsRouter = express.Router();
 
-columnsRouter.post("/:id", protect, validateBody(addColumnSchema), addColumn)
-  // .get("/:id", protect, getColumns)
-  .get("/:id", protect, getColumnById)
-  .patch("/:id", protect, validateBody(updateColumnSchema), updateColumn)
-  .delete("/:id", protect, deleteColumn);
+columnsRouter
+   .post("/:id", protect, validateBody(addColumnSchema), addColumn)
+   .patch("/:id", protect, validateBody(updateColumnSchema), updateColumn)
+   .delete("/:id", protect, deleteColumn);
 
 module.exports = columnsRouter;
