6bf2b5ea7dad5b2a06d7d801600f57434be4f252
ledavto <ser_ega@ukr.net>
2024-03-08 22:49:37 +0200

    Update cardsRouter.js

--- routes/cardsRouter.js
+++ routes/cardsRouter.js
@@ -10,7 +10,6 @@ const { protect, validateBody, validateId } = require("../middlewares");
 const {
    addCardSchema,
    updateCardSchema,
-   moveCardSchema,
 } = require("../schemas");
 
 const cardsRouter = express.Router();
@@ -19,6 +18,6 @@ cardsRouter
    .post("/:id", protect, validateBody(addCardSchema), addCard)
    .patch("/:id", protect, validateBody(updateCardSchema), updateCard)
    .delete("/:id", protect, validateId, deleteCard)
-   .patch("/move/:id", protect, moveCard);
+   .patch("/move/:id", protect, validateId, moveCard);
 
 module.exports = cardsRouter;
